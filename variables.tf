ac99436b3cd022b2577ccb7150b118963b68528c
Peet Gouws <peetg@discovery.co.za>
2018-11-07 09:22:53 +0200

    aws credential test

--- variables.tf
+++ variables.tf
@@ -23,6 +23,7 @@ variable "aws_region" {
 variable "aws_amis" {
   default = {
     eu-west-1 = "ami-674cbc1e"
+    eu-west-2 = "ami-674cbc1e"
     us-east-1 = "ami-1d4e7a66"
     us-west-1 = "ami-969ab1f6"
     us-west-2 = "ami-8803e0f0"
